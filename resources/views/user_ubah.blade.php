49b4a22a3e95cf4742027e3de713c6af2ead81ed
Manggala Kagendra Zuhdi <kagendra147@gmail.com>
2024-10-02 00:25:25 +0700

    Changes to be committed: 	modified:   app/Http/Controllers/UserController.php 	new file:   app/Http/Controllers/WelcomeController.php 	modified:   composer.json 	modified:   composer.lock 	new file:   resources/views/layouts/breadcrumb.blade.php 	new file:   resources/views/layouts/footer.blade.php 	new file:   resources/views/layouts/header.blade.php 	new file:   resources/views/layouts/sidebar.blade.php 	new file:   resources/views/layouts/template.blade.php 	deleted:    resources/views/template.blade.php 	new file:   resources/views/user/create.blade.php 	new file:   resources/views/user/edit.blade.php 	new file:   resources/views/user/index.blade.php 	new file:   resources/views/user/show.blade.php 	modified:   resources/views/user_ubah.blade.php 	modified:   resources/views/welcome.blade.php 	modified:   routes/web.php

--- resources/views/user_ubah.blade.php
+++ resources/views/user_ubah.blade.php
@@ -9,7 +9,7 @@
     <h1>Form Ubah Data User</h1>
     <a href="/user">kembali</a>
     <br><br>
-    <form method="post" action="/user/tambah/simpan//{{$data->user_id}}">
+    <form method="post" action="/user/ubah/simpan/{{$data->user_id}}">
 
         {{csrf_field()}}
         {{method_field('PUT')}}
